e052722c8ab63d294b10c919893962d338a11e7e
Philipp Zeller <38989128+phzeller@users.noreply.github.com>
2020-12-08 12:54:17 +0100

    Day 1: solved part 2

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -6,16 +6,33 @@ def calculateSummands(input):
             if i + j == 2020 and indexI != indexJ:
                 return (i, j)
 
+def calculateThreeSummands(input):
+    for indexI, i in enumerate(input):
+        for indexJ, j in enumerate(input):
+            for indexK, k in enumerate(input):
+                if i + j + k == 2020 and indexI != indexJ != indexK:
+                    return i, j, k
+
 def main():
     with open("input") as input_file:
         input = input_file.read().splitlines()
 
     input = list(map(int, input))
+
+    # PART 1
     summands = calculateSummands(input)
-    print("Target = {} * {} = {}".format(summands[0], summands[1], summands[0] * summands[1]))
+    if len(summands) == 2:
+        print("Part 1 Target = {} * {} = {}".format(summands[0], summands[1], summands[0] * summands[1]))
+    else:
+        print("Part1: no solution found")
 
-
-
+    # PART 2
+    threeSummands = calculateThreeSummands(input)
+    if len(threeSummands) == 3:
+        print("Part 2 Target = {} * {} * {} = {}".format(threeSummands[0], threeSummands[1], threeSummands[2],
+                                                         threeSummands[0] * threeSummands[1] * threeSummands[2]))
+    else:
+       print("Part1: no solution found")
 
 if __name__ == "__main__":
     main()
